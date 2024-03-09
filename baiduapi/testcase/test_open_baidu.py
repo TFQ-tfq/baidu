2baf6a1d6e874f0eba7a2b1b535076d5308158ac
tangfq <tangfangquan@mbse.com.cn>
2024-03-09 22:55:14 +0800

    11

--- baiduapi/testcase/test_open_baidu.py
+++ baiduapi/testcase/test_open_baidu.py
@@ -5,5 +5,13 @@ from baseapi.baseapi import openbaidu
 def test_open_baidu():
     result = openbaidu()
 
+def test_open_baidu01():
+    result = openbaidu()
+
+def test_open_baidu01():
+    result = openbaidu()
+
+def test_open_baidu01():
+    result = openbaidu()
 # if __name__ == '__main__':
 #     pytest.main(['-vs'])
